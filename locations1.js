56e898c83a684849f57d23ab1f014b86867d86a3
MetriPallavi <94187570+metripallavi@users.noreply.github.com>
2024-01-12 12:16:51 +0100

    Add files via upload
    Javascript - Routes nodemodules

---
+++ locations1.js
@@ -0,0 +1,6 @@
+// @desc Get all locations 
+// @route GET /api/v1/locations
+// @acess Public
+exports.getLocations = (req, res, next) => {
+    res.send('Hello');
+} 
--- server.js
+++ server.js
@@ -1,44 +1,23 @@
 const path = require('path');
-const express = require('express');
+const express= require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
-const connectDB = require('./config/db');
-const geocoder = require('./utils/geocoder.js');
-// load env vars
-dotenv.config({path:'./config/config.env'});
-console.log(process.env.MONGO_URI);
 
-// connect to database
-connectDB();
+//load env vars
+dotenv.config({path: './config/config.env'});
 
-const app= express();
+const app = express();
 
-// Body parser
+//Body parser 
 app.use(express.json());
 
-// Enable cors
+//Enable cors
 app.use(cors());
 
-// Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+//Routes 
+app.use('/api/v1/locations', require('./routes/locations'));
 
-// Routers
-app.use('/api/v1/volunteers', require('./routes/volunteers'));
-app.route('/api/v1/resolve').post(async function(req, res) {
-    try {                
-        //console.log(req.body.addr);
-        const loc = await geocoder.geocode(req.body.addr);
-        return res.status(200).json({
-            type: 'Point',
-            coordinates: [loc[0].longitude, loc[0].latitude],
-            formattedAddress: loc[0].formattedAddress
-        });
-    }catch(err) {
-        console.error(err);
-        res.status(500).json({ error: 'Server error1' });
-    }
-});
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
-
+app.listen(PORT, () => console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
